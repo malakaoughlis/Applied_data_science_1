33c69f33d25bd3d1bd0ddfc8e6cf508d8a613332
malak aoughlis <malak.aoughlis@gmail.com>
2025-03-05 23:31:56 +0000

    Update statistics_and_trends.py

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -178,15 +178,14 @@ def writing(moments, col):
     """
 
     if moments:
-        mean, stddev, skew, excess_kurtosis = moments  # Déballer les valeurs retournées
+        mean, stddev, skew, excess_kurtosis = moments 
 
         print(f'\nFor the attribute " {col} " :')
         print(f'Mean = {mean:.2f}, '
               f'Standard Deviation = {stddev:.2f}, '
               f'Skewness = {skew:.2f}, and '
               f'Excess Kurtosis = {excess_kurtosis:.2f}.')
-
-        # Interprétation de skewness et kurtosis
+        
         if skew > 0:
             skewness_desc = 'right-skewed'
         elif skew < 0:
